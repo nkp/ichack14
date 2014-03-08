3ece1022908c160df8e46dccaa9da15a93b2905b
Kacper Zylka <kacper.zylka12@imperial.ac.uk>
2014-03-08 18:16:53 +0000

    Small changes in dataAPI

--- src/org/ichack/glassmove/dataAPI/Property.java
+++ src/org/ichack/glassmove/dataAPI/Property.java
@@ -2,4 +2,32 @@ package org.ichack.glassmove.dataAPI;
 
 public class Property {
 
+	String street;
+	int numBedrooms;
+	String status;
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public int getNumBedrooms() {
+		return numBedrooms;
+	}
+
+	public void setNumBedrooms(int numBedrooms) {
+		this.numBedrooms = numBedrooms;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
 }
